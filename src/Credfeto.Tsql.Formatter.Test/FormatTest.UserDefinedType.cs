f9679180da050af2bc7448a6d0069be1548fc146
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 04:59:03 +0000

    Cleanup: FormatTest.UserDefinedType.cs

--- src/Credfeto.Tsql.Formatter.Test/FormatTest.UserDefinedType.cs
+++ src/Credfeto.Tsql.Formatter.Test/FormatTest.UserDefinedType.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Credfeto.Tsql.Formatter.Test;
@@ -8,8 +8,10 @@ public sealed partial class FormatTest
     [Fact]
     public Task FormatUserDefinedTypeAsync()
     {
-        const string source = "SET QUOTED_IDENTIFIER ON\nGO\n\nSET ANSI_NULLS ON\nGO\n\nCREATE TYPE [Accounts].[AccountConfigType]\nFROM NVARCHAR(MAX)\nGO\n\n\n";
-        const string expected = "SET QUOTED_IDENTIFIER ON;\n\n\nGO\nSET ANSI_NULLS ON;\n\n\nGO\nCREATE TYPE [Accounts].[AccountConfigType]\n    FROM NVARCHAR (MAX);\n\n\nGO\n\n\n";
+        const string source =
+            "SET QUOTED_IDENTIFIER ON\nGO\n\nSET ANSI_NULLS ON\nGO\n\nCREATE TYPE [Accounts].[AccountConfigType]\nFROM NVARCHAR(MAX)\nGO\n\n\n";
+        const string expected =
+            "SET QUOTED_IDENTIFIER ON;\n\n\nGO\nSET ANSI_NULLS ON;\n\n\nGO\nCREATE TYPE [Accounts].[AccountConfigType]\n    FROM NVARCHAR (MAX);\n\n\nGO\n\n\n";
 
         return this.ReformatAsync(source: source, expected: expected);
     }
